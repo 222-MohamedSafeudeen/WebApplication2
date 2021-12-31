560507d0fad658d9b463ed81e2557e7def6411c0
mohamedsafeudeen <mohammed.safeudeen@inceptionlabs.in>
2021-12-31 15:22:51 +0530

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Customer> customers = new List<Customer>();
+            customers.Add(new Customer() { CustomerID = 123, CustomerName = "Mohamed", CustomerAddress = "TamilNadu" });
+            customers.Add(new Customer() { CustomerID = 456, CustomerName = "Thousif", CustomerAddress = "Bihar" });
+            customers.Add(new Customer() { CustomerID = 789, CustomerName = "Saif", CustomerAddress = "Uttarpradesh" });
+
+            ViewData["Customer"] = customers;
+            return View();
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerAddress { get; set; }
+    }
+}
